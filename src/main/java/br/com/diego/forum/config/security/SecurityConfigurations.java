a8fbf94271e3628027affc85219045c240253084
Diego Couto <die_go0107@yahoo.com.br>
2020-07-12 18:39:56 -0300

    liberando as rotas Get

--- src/main/java/br/com/diego/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/diego/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.diego.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,5 +13,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	
+	//Configuraçãoes de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
+	
+	//Configuração de Autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/topico/*").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll();
+	}
+	
+	//Configurações de recursos estáticos (javascript, css, imagens...)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
 	
 }
