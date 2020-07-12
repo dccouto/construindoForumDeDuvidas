1ffab76c413137e2ab0d58c6d4e11f84f8ed5b16
Diego Couto <die_go0107@yahoo.com.br>
2020-07-12 18:27:42 -0300

    Baixando e habilitando o spring security

---
+++ src/main/java/br/com/diego/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,13 @@
+package br.com.diego.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+	
+	
+}
